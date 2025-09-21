90cb43d2b1f03ae0801fe414fda64e85771cd3ee
pratapganji <pratap.ganji@gmail.com>
2025-09-20 23:08:34 -0500

    Update wipvip.java

--- wipvip.java
+++ wipvip.java
@@ -1,13 +1,33 @@
-Subject: Jira details for HTTPS Migration – CRM Olympus API
+Subject: JIRA Details & API List for CRM Olympus HTTPS Migration
 
 Hi Param,
 
-As discussed, please find the Jira documenting the HTTPS migration changes for CRM Olympus API:
-	•	Jira ID: C167969S-104428 – Target Server URL to HTTPS for CRM Olympus API
-	•	Scope: Updating CRM Olympus API endpoints to use HTTPS for VIP and WIP URLs.
-	•	Progress: VIP/WIP HTTPS URLs created and updated; deployment/configuration changes done; currently resolving Jenkins pipeline build issue.
-	•	Impacted CRM Flows: All CRM Olympus API flows using VIP/WIP endpoints.
+As requested, please find below the details of the APIs impacted by the CRM Olympus HTTPS migration, along with the updated endpoints for QE testing.
 
-We will continue updating this Jira with build and deployment progress. Once the pipeline is unblocked, we’ll proceed with UAT deployment and QE validation.
+JIRA References:
+	•	[C167969S-104428] Target Server URL to HTTPS for CRM Olympus API
+	•	[C167969S-117341] Improve Code Coverage for CRM Olympus API
+	•	[C167969S-117342] Fix Vulnerabilities in CRM Olympus API
 
-Thanks,
+API Endpoints Updated to HTTPS:
+
+DEV (VIP/WIP):
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/health
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/publishCallReportData
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/publishClientActionItemData
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/publishClientMeetingData
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/publishClientProductivityMetricsDetails
+	•	https://olympus.api.crmtoolympusnew.dev.cloudgsl.nam.nsroot.net/crm_olympus/clientMeetingProtoTemplate
+
+UAT (VIP/WIP):
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/health
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/publishCallReportData
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/publishClientActionItemData
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/publishClientMeetingData
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/publishClientProductivityMetricsDetails
+	•	https://olympus.api.crmtoolympusnew.uat.cloudgsl.nam.nsroot.net:2023/crm_olympus/clientMeetingProtoTemplate
+
+Additional Notes:
+	•	VIP and WIP URLs for both DEV and UAT have been created and updated.
+	•	Currently, we are resolving Jenkins build pipeline issues; once resolved, these changes will be deployed to UAT.
+	•	We will notify QE as soon as the changes are available in UAT for validation.
