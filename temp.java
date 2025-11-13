e5b4644cada5482af67d83192e767abb3fab0b2b
pratapganji <pratap.ganji@gmail.com>
2025-11-12 19:57:39 -0600

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,5 +1,21 @@
-apiVersion: v2
-name: olympus-sb-user-consumption
-description: Helm chart for olympus-sb-user-consumption batch process on OpenShift
-type: application
-version: 0.1.0
+app:
+  name: olympus-sb-user-consumption
+
+image:
+  repository: docker-icg-dev-local.artifactory.citigroup.net/icg-isg-olympus/olympus-sb-user-consumption
+  tag: "1.0.0"
+  pullPolicy: IfNotPresent
+
+cron:
+  schedule: "0 3 * * 1"
+
+resources:
+  requests:
+    cpu: "500m"
+    memory: "1Gi"
+  limits:
+    cpu: "1500m"
+    memory: "3Gi"
+
+env:
+  PROFILE: "dev"
