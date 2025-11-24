e1e11f323268e4af28bc2ce0150c33133bb6ae1a
pratapganji <pratap.ganji@gmail.com>
2025-11-24 15:53:33 -0600

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,7 +1,8 @@
 https://github.com/jasypt/jasypt/releases/download/jasypt-1.9.3/jasypt-1.9.3-dist.zip
 
 
-java -cp jasypt.jar org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI \
+java -cp jasypt-1.9.3.jar \
+  org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI \
   input="changeit" \
-  password="d7+t6Olhoif3G10hWHXj0VZeo0Nbtsu7NB7MitaUEtw=" \
+  password="d7+t6OIhoif3G10hWHXj0VZeo0Nbtsu7NB7MitaUEtw=" \
   algorithm=PBEWITHHMACSHA512ANDAES_256
