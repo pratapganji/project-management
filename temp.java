e66ce4d6ef798f0a0949f8adf1695f6b043f4b09
pratapganji <pratap.ganji@gmail.com>
2025-11-24 21:46:30 -0600

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,8 +1,14 @@
-https://github.com/ulisesbocchio/jasypt-spring-boot/releases/download/3.0.5/jasypt-3.0.5-dist.zip
+import org.jasypt.util.text.AES256TextEncryptor;
 
+public static void main(String[] args) {
 
-java -cp jasypt-1.9.3.jar \
-  org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI \
-  input="changeit" \
-  password="d7+t6OIhoif3G10hWHXj0VZeo0Nbtsu7NB7MitaUEtw=" \
-  algorithm=PBEWITHHMACSHA512ANDAES_128
+    // TEMP ENCRYPTION
+    AES256TextEncryptor encryptor = new AES256TextEncryptor();
+    encryptor.setPassword("d7+t6OIhoif3G10hWHXj0VZeo0Nbtsu7NB7MitaUEtw="); // your key
+    String encrypted = encryptor.encrypt("changeit"); // your real JKS password
+    System.out.println("ENC(" + encrypted + ")");
+
+    if (true) return; // stop app after printing encrypted text
+
+    SpringApplication.run(CrmDataIngestToOlympusApplication.class, args);
+}
