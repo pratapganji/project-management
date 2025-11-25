141fb82bfa43f4cc02da3908f7dc5d37608bb163
pratapganji <pratap.ganji@gmail.com>
2025-11-24 22:18:26 -0600

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,14 +1,1 @@
-import org.jasypt.util.text.AES256TextEncryptor;
-
-public static void main(String[] args) {
-
-    // TEMP ENCRYPTION
-    AES256TextEncryptor encryptor = new AES256TextEncryptor();
-    encryptor.setPassword("d7+t6OIhoif3G10hWHXj0VZeo0Nbtsu7NB7MitaUEtw="); // your key
-    String encrypted = encryptor.encrypt("changeit"); // your real JKS password
-    System.out.println("ENC(" + encrypted + ")");
-
-    if (true) return; // stop app after printing encrypted text
-
-    SpringApplication.run(CrmDataIngestToOlympusApplication.class, args);
-}
+a
