4fc94a6c63f86835ed21d39ae34eb71705d85e66
pratapganji <pratap.ganji@gmail.com>
2025-11-25 10:52:56 -0600

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,22 +1,28 @@
 def read_input_params():
     parser = argparse.ArgumentParser()
+
+    # env: optional, default from environment variable "env"
     parser.add_argument(
         "--env",
-        required=False,                     # ← was True
-        default=os.getenv("env"),           # ← take from env var when not passed
-        help="Environment to run the script (dev, uat, prod)"
+        required=False,
+        default=os.getenv("env") or os.getenv("ENVIRONMENT"),
+        help="Environment to run the script (dev, uat, prod)",
     )
+
     parser.add_argument(
         "--api_source",
         required=False,
-        default="rest",                     # we always use rest in Openshift
-        help="Environment to run the script (rest, jdbc, bulk, ...)"
+        default="rest",          # we just use rest in Openshift
+        help="Environment to run the script (rest, jdbc, bulk)",
     )
-    parser.add_argument("--from_date", required=False, help="...")
-    parser.add_argument("--to_date",   required=False, help="...")
+    parser.add_argument("--from_date", required=False, help="Start date [yyyy-mm-dd]")
+    parser.add_argument("--to_date",   required=False, help="To date [yyyy-mm-dd]")
 
     args = vars(parser.parse_args())
 
-    env = args["env"]                      # ← use this instead of os.getenv()
+    env = args["env"]
     api_source = args["api_source"]
-    ...
+    from_date = args["from_date"]
+    to_date = args["to_date"]
+
+    return env, from_date, to_date, datetime.date.today().strftime("%Y-%m-%d"), api_source
