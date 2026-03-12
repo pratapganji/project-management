49f215537a53f461d7d05903cd4c2935d77048c0
pratapganji <pratap.ganji@gmail.com>
2026-03-12 09:21:47 -0500

    Update checklist.java

--- checklist.java
+++ checklist.java
@@ -1,14 +1,1 @@
-Hi Paresh,
-
-I hope you are doing well.
-
-I realized that I used the PTS project ID 20250109942 for 2nd Jan under the period 28-Dec-2025 to 03-Jan-2026, which appears to be incorrect.
-
-Could you please help correct this entry in PTS?
-
-Please let me know if you need any information from my side.
-
-Thank you.
-
-Best regards,  
-Pratap
+https://artifactrepository.citigroup.net/ui/native/docker-enterprise-prod-local/developersvcs-python-ai/redhat-python-rhel8
