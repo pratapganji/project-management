2e03748a7e38020f864aac4e56cc5e9c52073b5e
Balaji Vinnakota <balajivinnakota2@gmail.com>
2026-05-14 21:50:03 +0530

    Update unifiedApi.sql

--- unifiedApi.sql
+++ unifiedApi.sql
@@ -9,3 +9,33 @@ SELECT
     '[' || DATA_TYPE || ']' AS DT
 FROM A167969NURAREC.validation_config_rules
 WHERE HIERARCHY_PATH LIKE '%2052a%'
+
+
+
+SELECT *
+FROM A167969NURAREC.validation_config_rules
+WHERE
+(
+    (
+        HIERARCHY_PATH = '2052a~Debt'
+        AND DATA_TYPE = 'hop_validation'
+    )
+    OR
+    (
+        HIERARCHY_PATH = '2052a~Loans'
+        AND DATA_TYPE = 'generic_validation'
+    )
+)
+AND
+NOT
+(
+    (
+        HIERARCHY_PATH = '2052a~Debt'
+        AND DATA_TYPE = 'hop_validation'
+    )
+    OR
+    (
+        HIERARCHY_PATH = '2052a~Loans'
+        AND DATA_TYPE = 'generic_validation'
+    )
+);
