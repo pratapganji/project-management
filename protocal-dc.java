5518c8e8c13db5b56865171ca4ac2ee70f19c8d9
pratapganji <pratap.ganji@gmail.com>
2026-08-12 11:12:40 -0500

    Update protocal-dc.java

--- protocal-dc.java
+++ protocal-dc.java
@@ -1,6 +1,208 @@
 
 
 
+
+{
+    "status": "SUCCESS",
+    "resultBody": {
+        "entityData": {
+            "name": "$.body",
+            "displayName": "body",
+            "description": "body",
+            "absolutePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body",
+            "relativePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body",
+            "breadcrumbPath": "Information Services Group/Citihawk/dictionary/MOTradeMessage/current/body",
+            "logicalName": "$.body",
+            "physicalName": "$.body",
+            "relativePaths": [
+                "ISG/CHWK/dictionary/mo-trade-message/current/$.body"
+            ],
+            "links": [
+            ],
+            "tagAssociated": [
+            ],
+            "commentsAnchor": {
+                "anchorUuid": "d3b2dac0-7197-3814-98e2-8e0cec6eb0d0",
+                "version": "3"
+            },
+            "cdeAttributes": [
+            ],
+            "ddeAttributes": [
+            ],
+            "conceptType": "Child Concept",
+            "attributes": [
+                {
+                    "name": "VSPDate",
+                    "displayName": "VSPDate",
+                    "absolutePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/VSPDate",
+                    "relativePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/VSPDate",
+                    "logicalName": "VSPDate",
+                    "physicalName": "VSPDate",
+                    "relativePaths": [
+                        "ISG/CHWK/dictionary/mo-trade-message/current/$.body/VSPDate"
+                    ],
+                    "tagAssociated": [
+                    ],
+                    "cdeNames": [
+                    ],
+                    "definition": "VSPDate",
+                    "dataType": "ExternalDataTypeReleaseReference",
+                    "nullable": true,
+                    "keys": {
+                        "foreign": false,
+                        "unique": false,
+                        "primary": false,
+                        "non-unique": false
+                    },
+                    "dataTypeDefinition": {
+                        "typeName": "ExternalDataTypeReleaseReference",
+                        "business": "ISG",
+                        "provider": "CHWK",
+                        "dictionary": "fix-fields",
+                        "releaseFlag": false,
+                        "dataType": "$defs.VSPDate",
+                        "type": "DATA_TYPE_REF",
+                        "absolutePath": "ISG/CHWK/dictionary/fix-fields/_v1",
+                        "releaseVersion": "v1"
+                    }
+                },
+                {
+                    "name": "AccountGrp",
+                    "displayName": "AccountGrp",
+                    "absolutePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/AccountGrp",
+                    "relativePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/AccountGrp",
+                    "logicalName": "AccountGrp",
+                    "physicalName": "AccountGrp",
+                    "relativePaths": [
+                        "ISG/CHWK/dictionary/mo-trade-message/current/$.body/AccountGrp"
+                    ],
+                    "tagAssociated": [
+                    ],
+                    "cdeNames": [
+                    ],
+                    "definition": "AccountGrp",
+                    "dataType": "ConceptReleaseReference",
+                    "nullable": true,
+                    "keys": {
+                        "foreign": false,
+                        "unique": false,
+                        "primary": false,
+                        "non-unique": false
+                    },
+                    "dataTypeDefinition": {
+                        "typeName": "ConceptReleaseReference",
+                        "business": "ISG",
+                        "provider": "CHWK",
+                        "dictionary": "account-grp",
+                        "releaseFlag": false,
+                        "concept": "$",
+                        "type": "CONCEPT_REF",
+                        "absolutePath": "ISG/CHWK/dictionary/account-grp/_v1/$",
+                        "releaseVersion": "v1"
+                    }
+                },
+                {
+                    "name": "IncompleteTradeFlag",
+                    "displayName": "IncompleteTradeFlag",
+                    "absolutePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/IncompleteTradeFlag",
+                    "relativePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body/IncompleteTradeFlag",
+                    "logicalName": "IncompleteTradeFlag",
+                    "physicalName": "IncompleteTradeFlag",
+                    "relativePaths": [
+                        "ISG/CHWK/dictionary/mo-trade-message/current/$.body/IncompleteTradeFlag"
+                    ],
+                    "tagAssociated": [
+                    ],
+                    "cdeNames": [
+                    ],
+                    "definition": "IncompleteTradeFlag",
+                    "dataType": "ExternalDataTypeReleaseReference",
+                    "nullable": true,
+                    "keys": {
+                        "foreign": false,
+                        "unique": false,
+                        "primary": false,
+                        "non-unique": false
+                    },
+                    "dataTypeDefinition": {
+                        "typeName": "ExternalDataTypeReleaseReference",
+                        "business": "ISG",
+                        "provider": "CHWK",
+                        "dictionary": "fix-fields",
+                        "releaseFlag": false,
+                        "dataType": "$defs.IncompleteTradeFlag",
+                        "type": "DATA_TYPE_REF",
+                        "absolutePath": "ISG/CHWK/dictionary/fix-fields/_v1",
+                        "releaseVersion": "v1"
+                    }
+                }
+            ],
+            "childConcepts": [
+            ],
+            "siblingItems": [
+                {
+                    "name": "$.body",
+                    "displayName": "body",
+                    "absolutePath": "ISG/CHWK/dictionary/mo-trade-message/current/$.body",
+                    "entityType": "CONCEPT"
+                }
+            ],
+            "synonyms": [
+            ],
+            "urn": "data://citifix-dac.citihawk.isg.icg/model/v1/mo-trade-message#/properties/body",
+            "dictionaryUrn": "data://citifix-dac.citihawk.isg.icg/model/v1/mo-trade-message",
+            "referencedConcepts": [
+            ],
+            "properties": [
+                {
+                    "propertyName": "isNested",
+                    "code": "nested",
+                    "value": [
+                        "true"
+                    ]
+                }
+            ],
+            "relationships": {
+                "from": [
+                ],
+                "to": [
+                ]
+            },
+            "gdeAttributes": [
+            ],
+            "hasAdsDesignation": false
+        },
+        "entityType": "CONCEPT"
+    },
+    "statusCode": 0,
+    "statusMsg": null,
+    "statusDetail": {
+        "serviceErrorMsg": "",
+        "apiErrorMsg": "",
+        "traceId": "6a7c7e6e0628de14ec431d9c15c00bca",
+        "spanId": "ec431d9c15c00bca",
+        "suggestion": null,
+        "queryParams": null,
+        "uiServicePodName": "ui-service-vep-79c669dc59-lwx7n"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import lombok.Data;
 
 @Data
